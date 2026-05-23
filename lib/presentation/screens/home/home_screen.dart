bf855980147296402f67d1ea9c38e206a73cf38e
Anthonny <tonysolorzano880@gmail.com>
2026-05-23 11:02:45 +0200

    feat(screens): se crearon nuevas pantallas
    - Pantalla home_screen para pantalla principal
    - Pantalla basic_prompt_screen la pantalla que navega a la conversacion
      con Gemini
    - Se implemento una arquitectura Domain-Driven Design

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Google Gemini"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: const CircleAvatar(
+              backgroundColor: Colors.red,
+              child: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
+            title: const Text("Prompt básico de Gemini"),
+            subtitle: const Text("Usando modelo flash"),
+            onTap: () => context.push('/basic-prompt'),
+          )
+        ],
+      ),
+    );
+  }
+}
