5156057f4e9a36a4f2b3a28b14afd15579e7568c
Anthonny <tonysolorzano880@gmail.com>
2026-05-23 11:06:35 +0200

    feat(routes): se implemento la navegacion con GoRouter

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:gemini_chat_app/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/basic-prompt',
+      builder: (context, state) => const BasicPromptScreen(),
+    )
+  ],
+);
