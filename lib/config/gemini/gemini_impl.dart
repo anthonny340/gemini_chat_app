0937c020358c8cb07726bdc30b7894a793cc6328
Anthonny Sacheri <tonysolorzano880@gmail.com>
2026-06-05 21:58:52 +0200

    feat: implementacion del getChatStream

--- lib/config/gemini/gemini_impl.dart
+++ lib/config/gemini/gemini_impl.dart
@@ -79,4 +79,72 @@ class GeminiImpl {
       }
     }
   }
+
+  Stream<String> getChatStream(
+    String prompt,
+    String chatId, {
+    List<XFile> files = const [],
+  }) async* {
+    //! Multipart
+    final formData = FormData();
+    formData.fields.add(MapEntry('chatId', chatId));
+    formData.fields.add(MapEntry('prompt', prompt));
+
+    if (files.isNotEmpty) {
+      for (final file in files) {
+        // print(file.name);
+        formData.files.add(MapEntry(
+          'images',
+          await MultipartFile.fromFile(file.path, filename: file.name),
+        ));
+      }
+    }
+    final response = await _http.post(
+      '/chat-stream',
+      data: formData,
+      options: Options(responseType: ResponseType.stream),
+    );
+
+    final stream = response.data.stream as Stream<List<int>>;
+    String buffer = '';
+
+    await for (final bytes in stream) {
+      buffer += utf8.decode(bytes, allowMalformed: true);
+
+      final events = buffer
+          .split('\n\n'); // Cada chunk Esta separado asi, desde el backend
+
+      buffer = events.removeLast();
+
+      for (final event in events) {
+        if (!event.startsWith('data:')) continue;
+
+        final jsonString = event.replaceFirst('data:', '').trim();
+
+        final json = jsonDecode(
+            jsonString); // Para convertir el texto JSON en un objeto Dart
+
+        final chunk = json['chunk'];
+        final done = json['done'] == true;
+        final success = json['success'] == true;
+        final error = json['error'];
+
+        if (!success) {
+          print('''
+              ----Error en un chunk----
+              chunk: $chunk,
+              error: $error
+                ''');
+        }
+
+        if (chunk != null) {
+          yield chunk;
+        }
+
+        if (done) {
+          print('Stream finalizado');
+        }
+      }
+    }
+  }
 }
